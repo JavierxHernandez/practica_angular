dcd075b4d3b64361ca74134b436b41a69f59e443
Javier Hernández <javierxhernandezx@gmail.com>
2022-11-18 20:15:40 -0400

    feat: Class Binding.

---
+++ src/app/botones/botones.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  templateUrl: './botones.component.html',
+  styleUrls: ['./botones.component.css']
+})
+export class BotonesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  text_color:string='';
+
+  button_disabled:boolean=false;
+
+}
